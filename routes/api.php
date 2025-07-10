4c7ddec3f56c0d7e3484e6b6c677c7f0f3a982f4
goolm8325 <mohamedghool671@gmail.com>
2025-07-10 16:07:51 +0300

    modify routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 
-Route::post('/chat', [ChatController::class, 'handle']);
+// Route::post('/chat', [ChatController::class, 'handle']);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::middleware('role:admin')->apiResource('/sessions', SessionStateController::class);
     Route::apiResource('/orders', OrderController::class);
